da223bc92a3e0578d8a540164bea4d5b0e420b10
ifuka001 <ifuka001@ucr.edu>
2014-10-21 21:17:52 -0700

    added comments to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ void string_parsing(string String,vector<string> & char_array){
 
 
 int main(){
-	int returnstatus;
+	int status;
 	vector<string> char_array;
 	vector<string> pars_array;
 	char *login_name;
@@ -31,38 +31,45 @@ int main(){
 	int i;
 	int pid;
 	login_name = getlogin();
+	if(login_name == NULL){perror("getlogin");//check to see if we can obtain login info
+	strcpy(login_name,"");
+	}
 	char host_name[MAXHOSTNAMELEN];
-	gethostname(host_name,sizeof host_name);	
+	if(gethostname(host_name,sizeof host_name) == -1){perror("hostname");//check to see if we can obtain host name.
+	strcpy(host_name,"");
+	}	
 	string usr_input;
 	string usr_input2;	
 	
 
 	while(usr_input != "exit"){
 	beginning:
-	cout << login_name << "@" << host_name << "$"; 
+	cout << login_name << "@" << host_name << "$"; // outputs $ and waits for user input.
 	getline(cin, usr_input);
 	string usr_input2;
-	if(usr_input.empty()){
+	if(usr_input.empty()){//Checks to see if user just clicked enter and not input anything.
 		goto beginning;
 		}
 			
 		char * token;
-		token = strtok(&usr_input[0], "#");
+		token = strtok(&usr_input[0], "#");//this will remove the comment from user.
 		usr_input = token;
 	
 	int trav_str = usr_input.size();
 	int str_ctr  = 0;
 	for(str_ctr=0;str_ctr < trav_str;++str_ctr){
+												//this loop will go through to see what user has inputted (just single command,
+												//separated by ; or && or ||. (NOTE: this bash is not compatible with
+												//different arguments in one input. i.e. ls ; ls || ls && ls.
 		
-		
-		if(usr_input == "exit"){ 
+		if(usr_input == "exit"){ //will check if user wants to end program.
 		cout << "Exiting..."<< endl;
 		char_array.clear();
 		pars_array.clear();
-		//break;
 		goto end_of_while;
 		}
-		else if(usr_input[str_ctr] == ';'){
+		else if(usr_input[str_ctr] == ';'){//this will implement if the user has separated with a semicolon.
+
 			token = strtok(&usr_input[0], ";");
 				while(token != NULL){
 					pars_array.push_back(token);
@@ -84,7 +91,7 @@ int main(){
 				strcpy(argv[n], char_array[n].c_str());
 			}
 			argv[n] = NULL;
-			int pid = fork();
+			int pid = fork();//will fork program and let child run execvp and then waits for child process to tell parent it is done.
 			if(pid == -1){
 				perror("There was error with fork().");
 				exit(1);}
@@ -106,18 +113,22 @@ int main(){
 		
 
 
-		else if(usr_input[str_ctr] == '&' && usr_input[str_ctr+1] == '&'){
+		else if(usr_input[str_ctr] == '&' && usr_input[str_ctr+1] == '&'){//this will be implemented if user separates with &&.
+
 			token = strtok(&usr_input[0], "&&");
 				while(token != NULL){
 					pars_array.push_back(token);
 					token = strtok(NULL,"&&");
 				}
+			
 			int pars_size = pars_array.size();
 			int pars_ctr  = 0;		
 			
 	
-			for(pars_ctr =0; pars_ctr < pars_size; pars_ctr++){
+		for(pars_ctr =0; pars_ctr < pars_size; pars_ctr++){
+			
 				usr_input2 = pars_array[pars_ctr];
+				
 				string_parsing(usr_input2,char_array);			
 				int i = char_array.size();
 				char * argv[100];
@@ -129,37 +140,38 @@ int main(){
 				strcpy(argv[n], char_array[n].c_str());
 			}
 			argv[n] = NULL;
-			int pid = fork();
+			int pid = fork();//will fork program and let child run execvp and then waits for child process to tell parent it is done.
+		
 			if(pid == -1){
 				perror("There was error with fork().");
 				exit(1);}
 			else if(pid == 0){
-				if(-1 == execvp(argv[0],argv)){
+				if( -1 == execvp(argv[0],argv)){
 					perror("There was an error");	
+					exit(1);
 					}
 			}	
-			{
-				if( -1 ==  wait(&returnstatus))
-			{cout << "failed"<<endl;
-				usr_input.clear();
-				usr_input2.clear();
-				char_array.clear();
-				pars_array.clear();
-				goto end_of_while;
-				pars_ctr = pars_size;
-				break;
-				}
-					
-					
-			}			
-			char_array.clear();
+			wait(&status);
+			if(WIFEXITED(status)){
+			if(WEXITSTATUS(status) != 0){
+				
+			goto end_of_while;
 			}
+			}
+			
+					
+			char_array.clear();
+
+			}
+
+	
 		char_array.clear();
 		pars_array.clear();
 		goto end_of_while;
 		}
 		
-		else if(usr_input[str_ctr] == '|' && usr_input[str_ctr+1] == '|'){
+		else if(usr_input[str_ctr] == '|' && usr_input[str_ctr+1] == '|'){//This will be implemented if user inputs ||
+
 			token = strtok(&usr_input[0], "||");
 				while(token != NULL){
 					pars_array.push_back(token);
@@ -182,26 +194,25 @@ int main(){
 				strcpy(argv[n], char_array[n].c_str());
 			}
 			argv[n] = NULL;
-			int pid = fork();
+			int pid = fork();//will fork program and let child run execvp and then waits for child process to tell parent it is done.
 			if(pid == -1){
 				perror("There was error with fork().");
 				exit(1);}
 			else if(pid == 0){
 				if(-1 == execvp(argv[0],argv)){
 					perror("There was an error");	
-					}
+					exit(1);
+						}
 					
 			}	
-			else if(pid > 0){
-				if(-1 == wait(0)){perror("There was an error with wait().");
-					}
-				else{ 
-				usr_input.clear();
-				usr_input2.clear();
-				pars_array.clear();
-				char_array.clear();
-				goto end_of_while;}	
-			}			
+			wait(&status);
+			if(WIFEXITED(status)){
+			if(WEXITSTATUS(status) == 0){
+				
+			goto end_of_while;
+			}
+			}
+		
 			char_array.clear();
 			}
 		char_array.clear();
@@ -209,7 +220,10 @@ int main(){
 		goto end_of_while;
 		}
 		}
-		if(!usr_input.empty()){		string_parsing(usr_input,char_array);
+		if(!usr_input.empty()){	//when user just inputs no argument.
+	
+
+			string_parsing(usr_input,char_array);
 		
 			i = char_array.size();
 			char * argv[100];
@@ -221,7 +235,7 @@ int main(){
 				strcpy(argv[n], char_array[n].c_str());
 			}
 			argv[n] = NULL;
-			pid = fork();
+			pid = fork();//will fork program and let child run execvp and then waits for child process to tell parent it is done.
 			if(pid == -1){
 				perror("There was error with fork().");
 				exit(1);}
@@ -237,7 +251,7 @@ int main(){
 			char_array.clear();
 			pars_array.clear();
 			}
-	end_of_while:
+	end_of_while://this will go back to the beginning (while(!=exit)). It will also clear char_array and pars_array.
 	if(char_array.size() > 0){
 	char_array.clear();}
 	if(pars_array.size() > 0){
